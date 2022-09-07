a89feb72e50915616f923400e3118644d1aa1cf4
0xTas <admin@0xtas.dev>
2022-09-07 12:18:19 -0700

    Class III Gas Giant properties

--- src/stellar_bodies/planets.rs
+++ src/stellar_bodies/planets.rs
@@ -998,9 +998,9 @@ impl<'a> PlanetType<'a> {
 
                 let type_name: &str;
                 if ringed {
-                    type_name = "CHANGE ME (WITH RINGS)";
+                    type_name = "Ringed Class II Gas Giant";
                 }else {
-                    type_name = "CHANGE ME";
+                    type_name = "Class II Gas Giant";
                 };
 
                 let description: &str;
@@ -1010,7 +1010,7 @@ impl<'a> PlanetType<'a> {
                     description = "CHANGE ME";
                 };
 
-                let rarity: &str = "CHANGE ME";
+                let rarity: &str = "Rare";
 
                 const MIN_DIST: f64 = 4.41403;
                 const MAX_DIST: f64 = 841_899.0;
@@ -1052,6 +1052,83 @@ impl<'a> PlanetType<'a> {
                 let explorable: bool = is_explorable(surface_pressure, surface_temp, gravity);
 
                 return Self::CIIGG(PlanetTypeProperties {
+                    ringed,
+                    type_name,
+                    description,
+                    rarity,
+                    landable,
+                    explorable,
+                    dist_from_arrival,
+                    low_temp: MIN_TEMP,
+                    high_temp: MAX_TEMP,
+                    surface_temp,
+                    surface_pressure,
+                    radius,
+                    earth_masses,
+                    gravity,
+                    orbital_period,
+                    rotational_period,
+                });
+            },
+            "CIIIGG" => {
+                let ringed: bool = decide_ringed();
+
+                let type_name: &str;
+                if ringed {
+                    type_name = "Ringed Class III Gas Giant";
+                }else {
+                    type_name = "Class III Gas Giant";
+                };
+
+                let description: &str;
+                if ringed {
+                    description = "CHANGE ME (WITH RINGS)";
+                }else {
+                    description = "CHANGE ME";
+                };
+
+                let rarity: &str = "Common";
+
+                const MIN_DIST: f64 = 1.61936;
+                const MAX_DIST: f64 = 7_492_280.0;
+                let dist_from_arrival: f64 = rng().gen_range(MIN_DIST..=MAX_DIST);
+
+                const MIN_TEMP: i32 = 115;
+                const MAX_TEMP: i32 = 800;
+                let surface_temp: i32 = rng().gen_range(MIN_TEMP..=MAX_TEMP);
+
+                const MIN_PRESSURE: f64 = 0.0;
+                const MAX_PRESSURE: f64 = 20_305.728515625;
+                let surface_pressure: f64 = rng().gen_range(MIN_PRESSURE..=MAX_PRESSURE);
+
+                const MIN_RADIUS: f64 = 12_120.956;
+                const MAX_RADIUS: f64 = 77_849.944;
+                let radius: f64 = rng().gen_range(MIN_RADIUS..=MAX_RADIUS);
+
+                const MIN_MASSES: f64 = 4.296463;
+                const MAX_MASSES: f64 = 3_457.905762;
+                let earth_masses: f64 = rng().gen_range(MIN_MASSES..=MAX_MASSES);
+
+                const MIN_GRAVITY: f64 = 0.274524836901;
+                const MAX_GRAVITY: f64 = 199.304829579928;
+                let gravity: f64 = rng().gen_range(MIN_GRAVITY..=MAX_GRAVITY);
+
+                const MIN_ORBITAL: f64 = 0.018557870653;
+                const MAX_ORBITAL: f64 = 106_016_638.240329;
+                let orbital_range: f64 = rng().gen_range(MIN_ORBITAL..=MAX_ORBITAL);
+                let orbital_secs: f64 = orbital_range * 86400.0;
+                let orbital_period: Duration = Duration::from_secs_f64(orbital_secs);
+
+                const MIN_ROTATIONAL: f64 = 0.000862268519;
+                const MAX_ROTATIONAL: f64 = 218_760.441309931;
+                let rotational_range: f64 = rng().gen_range(MIN_ROTATIONAL..=MAX_ROTATIONAL);
+                let rotational_secs: f64 = rotational_range * 86400.0;
+                let rotational_period: Duration = Duration::from_secs_f64(rotational_secs);
+
+                let landable: bool = is_landable(surface_pressure, surface_temp);
+                let explorable: bool = is_explorable(surface_pressure, surface_temp, gravity);
+
+                return Self::CIIIGG(PlanetTypeProperties {
                     ringed,
                     type_name,
                     description,
