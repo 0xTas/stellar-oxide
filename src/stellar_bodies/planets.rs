f6d478c758b30c80aa30a0cbddf5712e1ec5e8ff
0xTas <admin@0xtas.dev>
2022-09-07 10:50:10 -0700

    Added Earth-like properties

--- src/stellar_bodies/planets.rs
+++ src/stellar_bodies/planets.rs
@@ -426,7 +426,80 @@ impl<'a> PlanetType<'a> {
                     orbital_period,
                     rotational_period,
                 });
-            }
+            },
+            "ELW" => {
+                let ringed: bool = decide_ringed();
+
+                let type_name: &str;
+                if ringed {
+                    type_name = "Ringed Earth-like World";
+                }else {
+                    type_name = "Earth-like World";
+                };
+
+                let description: &str;
+                if ringed {
+                    description = "CHANGE ME (WITH RINGS)";
+                }else {
+                    description = "CHANGE ME";
+                };
+
+                let rarity: &str = "Very Rare";
+
+                const MIN_DIST: f64 = 6.0;
+                const MAX_DIST: f64 = 736_306.0;
+                let dist_from_arrival: f64 = rng().gen_range(MIN_DIST..=MAX_DIST);
+
+                const MIN_TEMP: i32 = 260;
+                const MAX_TEMP: i32 = 497;
+                let surface_temp: i32 = rng().gen_range(MIN_TEMP..=MAX_TEMP);
+
+                const MIN_PRESSURE: f64 = 0.24206969;
+                const MAX_PRESSURE: f64 = 7.291643844066;
+                let surface_pressure: f64 = rng().gen_range(MIN_PRESSURE..=MAX_PRESSURE);
+
+                const MIN_RADIUS: f64 = 1_944.26225;
+                const MAX_RADIUS: f64 = 11_914.006;
+                let radius: f64 = rng().gen_range(MIN_RADIUS..=MAX_RADIUS);
+
+                const MIN_MASSES: f64 = 0.026;
+                const MAX_MASSES: f64 = 7.1;
+                let earth_masses: f64 = rng().gen_range(MIN_MASSES..=MAX_MASSES);
+
+                const MIN_GRAVITY: f64 = 0.279545410512;
+                const MAX_GRAVITY: f64 = 2.553103251365;
+                let gravity: f64 = rng().gen_range(MIN_GRAVITY..=MAX_GRAVITY);
+
+                const MIN_ORBITAL: f64 = 0.279575634606;
+                const MAX_ORBITAL: f64 = 271_840.426666667;
+                let orbital_range: f64 = rng().gen_range(MIN_ORBITAL..=MAX_ORBITAL);
+                let orbital_secs: f64 = orbital_range * 86400.0;
+                let orbital_period: Duration = Duration::from_secs_f64(orbital_secs);
+
+                const MIN_ROTATIONAL: f64 = 0.25040603397;
+                const MAX_ROTATIONAL: f64 = 5_591.70194340926;
+                let rotational_range: f64 = rng().gen_range(MIN_ROTATIONAL..=MAX_ROTATIONAL);
+                let rotational_secs: f64 = rotational_range * 86400.0;
+                let rotational_period: Duration = Duration::from_secs_f64(rotational_secs);
+
+
+                return Self::CHANGEME(PlanetTypeProperties {
+                    ringed,
+                    type_name,
+                    description,
+                    rarity,
+                    dist_from_arrival,
+                    low_temp: MIN_TEMP,
+                    high_temp: MAX_TEMP,
+                    surface_temp,
+                    surface_pressure,
+                    radius,
+                    earth_masses,
+                    gravity,
+                    orbital_period,
+                    rotational_period,
+                });
+            },
             _ => {
 
                 /* Template for efficiency purposes */
@@ -449,9 +522,9 @@ impl<'a> PlanetType<'a> {
 
                 // let rarity: &str = "CHANGE ME";
 
-                // const MIN_DIST: u32 = 
-                // const MAX_DIST: u32 = 
-                // let dist_from_arrival: u32 = rng().gen_range(MIN_DIST..=MAX_DIST);
+                // const MIN_DIST: f64 = 
+                // const MAX_DIST: f64 = 
+                // let dist_from_arrival: f64 = rng().gen_range(MIN_DIST..=MAX_DIST);
 
                 // const MIN_TEMP: i32 = 
                 // const MAX_TEMP: i32 = 
