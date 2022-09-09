b726ed8ce351ae3420d5596929a1999cfc43e127
0xTas <admin@0xtas.dev>
2022-09-09 09:54:47 -0700

    Replaced flush(), dur(), and sleep() with wait()

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::thread::sleep;
 use rand::Rng;
-use oasis::{cls, dur, flush, input, create_random_star, create_random_planet};
 use oasis::stellar_bodies::stars::Star;
 use oasis::stellar_bodies::planets::Planet;
+use oasis::{cls, wait, input, create_random_star, create_random_planet};
 
 
 /* Current goal is to generate random star systems
@@ -15,7 +14,7 @@ use oasis::stellar_bodies::planets::Planet;
 fn main() {
     loop {
         cls();
-        sleep(dur(420));
+        wait(420);
         println!("\n Another day out here on the edge of the Galaxy..\n");
         print!(" Would you like to boot into StellarOS? [Y/n] #> ");
 
@@ -29,13 +28,12 @@ fn main() {
                 print!(" >Loading");
                 for _ in 0..36 {
                     print!(".");
-                    flush();
                     let amount = rand::thread_rng().gen_range(42..=69);
-                    sleep(dur(amount));
+                    wait(amount);
                 };
-                sleep(dur(1500));
+                wait(1500);
                 println!("Done.");
-                sleep(dur(1000));
+                wait(1000);
                 println!("");
 
                 let current_star: Star = create_random_star();
@@ -43,42 +41,33 @@ fn main() {
                 // println!(" Your Star: {:#?}", current_star);
                 println!(" You've been here for quite some time already...");
                 println!(" Your current location is the system {}.\n It is a remote {} located on the far side of the galaxy.", current_star.name, current_star.stats().class_name);
-                flush();
-                sleep(dur(7000));
+                wait(7000);
                 println!(" Here are the details of the current system's star:\n {:#?}", star_stats);
-                flush();
-                sleep(dur(20000));
+                wait(20000);
                 
                 let random_planet: Planet = create_random_planet();
                 println!();
                 println!(" There is one interesting body in this system, you stayed around for a few days to examine it further.");
-                flush();
-                sleep(dur(2420));
+                wait(2420);
                 println!();
                 let get = random_planet.stats();
                 println!(" These are the details you discovered about the {} known as {}:\n {:#?}\n", get.type_name, random_planet.name, random_planet);
-                flush();
-                sleep(dur(60000));
+                wait(60000);
                 break;
             } else {
-                sleep(dur(700));
+                wait(700);
                 println!("\n Well, there isn't much else to do out here...\n");
-                flush();
-                sleep(dur(2500));
+                wait(2500);
                 print!(" *You sit and stare out the window of your cockpit at the stars for a while*");
-                flush();
-                sleep(dur(3000));
+                wait(3000);
                 for _ in 0..3 {
                     print!(". ");
-                    flush();
-                    sleep(dur(1000));
+                    wait(1000);
                 };
                 println!("");
-                flush();
-                sleep(dur(2420));
+                wait(2420);
                 println!("\n Goodbye!\n");
-                flush();
-                sleep(dur(2000));
+                wait(2000);
                 break;
             }
         } 
